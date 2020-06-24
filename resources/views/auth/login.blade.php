5ea777fe51f4dbba1957ab9b1015abf3f5aeca06
Edinson Samar <ev.samar.c@gmail.com>
2020-06-24 08:08:02 -0500

    Loging UI Advanced

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+
+<b-conteiner>
+    <b-row align-h="center">
+        <b-col cols="8">
+
+            <b-card title="Inicio de sesión">
+
+            <b-alert show>Por favor ingrese su contraseña</b-alert>
+
+                <b-form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                    <b-form-group
+                        label="Correo electrónico:"
+                        label-for="email"
+                        description="Nunca compartiremos tu email."
+                    >
+                        <b-form-input id="email"
+                            name="email"
+                            type="email"
+                            value="{{ old('email') }}" required autofocus
+                            placeholder="ejemplo@dominio.com"
+                        ></b-form-input>
+
+                    </b-form-group>
+
+                    <b-form-group 
+                        label="Contraseña:"
+                        label-for="password"
+                    >
+                        <b-form-input id="password"
+                            name="password"
+                            type="password"
+                            value="{{ old('password') }}" required
+                        ></b-form-input>
+
+                    </b-form-group>
+
+                    <b-form-group>
+                        <b-form-checkbox name="remember" {{ old('remember') ? 'checked' : '' }}                    >
+                            Recordar sesión
+                        </b-form-checkbox>
+                    </b-form-group>
+
+                    <b-button type="submit" variant="primary">Ingresar</b-button>
+                    <b-button href="{{ route('password.request') }}" variant="link">¿Olvidaste tu contraseña?</b-button>
+            
+                    </b-form>
+            
+            </b-card>
+
+
+                
+        </b-col>
+    </b-row>
+</b-conteiner>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+</head>
+<body>
+    <div id="app">
+
+        <b-navbar toggleable="sm" type="dark" variant="dark">
+            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+            <b-navbar-brand href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </b-navbar-brand>
+
+            <b-collapse id="nav-text-collapse" is-nav>
+
+                <b-navbar-nav class="ml-auto">
+                    <!-- @guest
+                        <b-nav-item href="{{ route('login') }}">Ingresar</b-nav-item>
+                        <b-nav-item href="{{ route('register') }}">Registrar</b-nav-item>
+                    @else -->
+
+                        <!-- Navbar dropdowns -->
+                        <b-nav-item-dropdown text="Username" right>
+                            <b-dropdown-item href="#">Cerrar sesión</b-dropdown-item>
+                        </b-nav-item-dropdown>
+
+                    <!-- @endguest -->
+
+                </b-navbar-nav>
+
+            </b-collapse>
+        </b-navbar>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
